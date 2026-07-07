40410ad92a4c93673e65f7e92bb424fd5d88c4c4
JosephAaron19 <ruizjoseph2002@gmail.com>
2026-07-07 10:45:17 -0500

    subida de cambios

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -8,10 +8,10 @@ class ApiService {
   // static const String baseUrl = 'http://10.0.2.2:8001/api';
 
   // 2. Para Simulador de iOS o Web:
-  // static const String baseUrl = 'https://apifincontrol.finatech.com.pe/api';
+  static const String baseUrl = 'https://apifincontrol.finatech.com.pe/api';
 
   // 3. Para Dispositivo Móvil Físico (conectado al mismo Wi-Fi que tu PC):
-    static const String baseUrl = 'http://192.168.1.128:8001/api';
+  //  static const String baseUrl = 'http://192.168.1.125:8001/api';
 
   // 4. URL de producción de la API:
   // static const String baseUrl = 'https://apifincontrol.finatech.com.pe/api';
